a518fd9e860123b7a69e1cbd7410dffbae7061bc
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2024-04-01 14:17:44 +0200

    created variables and reset file

--- ETA/src/App.jsx
+++ ETA/src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
+import './variables.css'
+import './reset.css'
 
 function App() {
 
   return (
     <>
+    <div className='window-container'>
+      <section className='current-money-container'>
+        <div className='current-money_div'>
+          <div className='current-money_title'>Available money</div>
+          <div className='current-money_number'>2914 NOK</div>
+        </div>
+      </section>
+      <section className='transaction-container'></section>
+    </div>
       
     </>
   )
