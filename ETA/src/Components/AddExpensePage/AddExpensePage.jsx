3805b6ee6feb35ab2eea49bbe8f8e3e3a0b6e83f
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2024-04-04 15:40:13 +0200

    designed add expenses page

--- ETA/src/Components/AddExpensePage/AddExpensePage.jsx
+++ ETA/src/Components/AddExpensePage/AddExpensePage.jsx
@@ -1,4 +1,5 @@
 import AddExpenseButton from "../AddExpenseButton/AddExpenseButton";
+import './AddExpensePage.css'
 
 export default function AddExpensePage(){
 
@@ -8,39 +9,45 @@ export default function AddExpensePage(){
 
 	return(
 		<>
-		<h1>Add Expenses</h1>
 
-		<form className="add-expense-form">
+		<section className="add-expense-page">
+			<h1>Add Expenses</h1>
 
-			<div className="form-input">
-				<label htmlFor="title">Title</label>
-				<input type="text" name="title" className="form-input_title"/>
-			</div>
-			<div className="form-input">
-				<label htmlFor="amount">Amount</label>
-				<input type="number" name="amount" className="form-input_amount"/>
-			</div>
-			<div className="form-input">
-				<label htmlFor="date">Date</label>
-				<input type="date" name="date" className="form-input_date"/>
+			<form className="add-expense-form">
 
-				<label htmlFor="date-today">Today?</label>
-				<input type="checkbox" name="date-today" className="form-input_date-today"/>
+				<div className="form-input">
+					<label htmlFor="title">Title</label>
+					<input type="text" name="title" className="form-input_title"/>
+				</div>
+				<div className="form-input">
+					<label htmlFor="amount">Amount</label>
+					<input type="number" name="amount" className="form-input_amount"/>
+				</div>
+				<span className="form-input_date-container">
+					<span className="form-input">
+						<label htmlFor="date">Date</label>
+						<input type="date" name="date" className="form-input_date" placeholder=""/>
+					</span>
+					
+					<span className="form-input">
+						<label htmlFor="date-today">Today?</label>
+						<input type="checkbox" name="date-today" className="form-input_date-today"/>	
+					</span>
+				</span>
 
-			</div>
+				<select className="select-category">
 
-			<select className="form-input">
+					<option selected disabled>Category</option>
+					<option value="housing">Housing</option>
+					<option value="grocery">Grocery</option>
+					<option value="transportation">Transportation</option>
+					<option value="clothes">Clothes</option>
+					<option value="other">Other</option>
+				</select>
 
-				<option selected disabled>Category</option>
-				<option value="housing">Housing</option>
-				<option value="grocery">Grocery</option>
-				<option value="transportation">Transportation</option>
-				<option value="clothes">Clothes</option>
-				<option value="other">Other</option>
-			</select>
-
-			<AddExpenseButton addingExpenseProp={addingExpense}/>
-		</form>
+				<AddExpenseButton addingExpenseProp={addingExpense}/>
+			</form>
+		</section>
 		</>
 	)
 }
