435d7f8d6452f315cfdd91274b4d6584466fa76d
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2024-04-09 12:22:29 +0200

    updated UI

--- ETA/src/App.jsx
+++ ETA/src/App.jsx
@@ -3,11 +3,12 @@ import './variables.css'
 import './reset.css'
 import AddExpensePage from './Components/AddExpensePage/AddExpensePage'
 import Dashboard from './Components/Dashboard/Dashboard'
+import { useRef } from 'react'
 
 function App() {
-
+  const IncomeOrExpenseContainer = useRef()
   const showIncomeOrExpense = ()=> {
-    
+    IncomeOrExpenseContainer.current.style.display = "flex"
   }
   return (
     <>
