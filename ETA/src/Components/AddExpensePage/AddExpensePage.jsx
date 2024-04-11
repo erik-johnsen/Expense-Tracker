9122a7164d8b2f25d81836041ea1233fe7dcae42
Erik Johnsen <141843146+erik-johnsen@users.noreply.github.com>
2024-04-11 14:57:53 +0200

    made a function that saves userinput

--- ETA/src/Components/AddExpensePage/AddExpensePage.jsx
+++ ETA/src/Components/AddExpensePage/AddExpensePage.jsx
@@ -1,23 +1,49 @@
+import { useRef, useState } from "react";
 import AddExpenseButton from "../AddExpenseButton/AddExpenseButton";
 import './AddExpensePage.css'
 
 export default function AddExpensePage(){
 
-	const addingExpense= ()=> {
+	const expenseForm = useRef(null)
 
+
+	const [error, setError] = useState({})
+	const [allExpenseItems, setAllExpenseItems] = useState([])
+
+	const valiForm = ()=>{
+		const clonedErrors = {...error}
+		clonedErrors.title = !expenseItem.title.trim() ? "Title is required!" : ""
+		clonedErrors.amount = !expenseItem.amount.trim() ? "Amount is required!" : ""
+		clonedErrors.date = !expenseItem.date.trim() ? "Date is required!" : ""
+		clonedErrors.category = !expenseItem.category.trim() ? "Category is required!" : ""
+
+		setError(clonedErrors)
+	}
+
+	const handleSubmit =(e)=> {
+		e.preventDefault()
+		const expenseFormData =  new FormData(expenseForm.current)
+
+		const newUser = {}
+		for(const [key, value] of expenseFormData.entries()){
+			newUser[key] = value
+		}
+		setAllExpenseItems((prev)=> [...prev, newUser])
+		console.log(allExpenseItems);
 	}
 
 	return(
 		<>
 
-		<section className="add-expense-page">
+		<section className="add-expense-page" >
 			<h1>Add Expenses</h1>
 
-			<form className="add-expense-form">
+			<form className="add-expense-form" onSubmit={handleSubmit} ref={expenseForm}>
 
 				<div className="form-input">
 					<label htmlFor="title">Title</label>
 					<input type="text" name="title" className="form-input_title"/>
+					
 				</div>
 				<div className="form-input">
 					<label htmlFor="amount">Amount</label>
@@ -35,7 +61,7 @@ export default function AddExpensePage(){
 					</span>
 				</span>
 
-				<select className="select-category">
+				<select className="select-category" name="category">
 
 					<option defaultValue={"Category"} disabled>Category</option>
 					<option value="housing">Housing</option>
@@ -45,7 +71,7 @@ export default function AddExpensePage(){
 					<option value="other">Other</option>
 				</select>
 
-				<AddExpenseButton addingExpenseProp={addingExpense}/>
+				<AddExpenseButton />
 			</form>
 		</section>
 		</>
